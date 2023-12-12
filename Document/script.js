b49f00dea30b82df4b64e345fd591a99f6429d4b
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-12-12 19:40:45 +0600

    index.html

--- Document/script.js
+++ Document/script.js
@@ -36,3 +36,58 @@ let changeText=()=>{
 }
 changeText();
 setInterval(changeText,3000)
+
+const circles=document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots =elem.getAttribute("data-dots")
+    var marked=elem.getAttribute("data-percent");
+    var percent=Math.floor(dots*marked/100);
+    var points="";
+    var rotate=360/dots;
+
+    for(let i=0 ; i < dots ;i++){
+        points +=`<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+    }
+
+    elem.innerHTML=points;
+
+    const poinstMarked=elem.querySelectorAll('.points')
+
+    for(let i=0; i< percent; i++){
+        poinstMarked[i].classList.add('marked')
+    }
+    
+})
+
+
+//active menue
+
+let menuLi=document.querySelectorAll('header ul li a')
+let section =document.querySelectorAll('section')
+
+function activeMenu(){
+    let len=section.length;
+    while(--len&& window.scrollY +97 < section[len].offsetTop){}
+    menuLi.forEach(sec=>sec.classList.remove("active"));
+    menuLi[len].classList.add("active")
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu)
+
+//stky header
+const header=document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY>50)
+})
+
+//menu icon
+
+let menuIcon=document.querySelector("#menu-icon")
+
+let navlist=document.querySelector(".navlist")
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle("bx-x")
+    navlist.classList.toggle("open")
+}
